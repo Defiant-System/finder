d4bd9a7c70a83bdf61bef28a450d8562721373dc
Hakan Bilgin <hbi99@hotmail.com>
2022-07-29 22:11:56 +0200

    Update spawn.js

--- src/js/spawn.js
+++ src/js/spawn.js
@@ -32,9 +32,9 @@
 				Spawn.find(".icon-resizer").val(value);
 
 				// temp
-				setTimeout(() => Spawn.find(`.ant-file_:nth(1)`).trigger("click"), 100);
-				setTimeout(() => Spawn.find(`.ant-file_:nth(16)`).trigger("click"), 200);
-				setTimeout(() => Spawn.find(`.toolbar-tool_[data-arg="-1"]`).trigger("click"), 500);
+				// setTimeout(() => Spawn.find(`.ant-file_:nth(1)`).trigger("click"), 100);
+				// setTimeout(() => Spawn.find(`.ant-file_:nth(16)`).trigger("click"), 200);
+				// setTimeout(() => Spawn.find(`.toolbar-tool_[data-arg="-1"]`).trigger("click"), 500);
 				
 				// setTimeout(() => Spawn.find(`.toolbar-tool_[data-arg="icons"]`).trigger("click"), 500);
 				// setTimeout(() => Spawn.find(`.toolbar-tool_[data-arg="columns"]`).trigger("click"), 1200);
